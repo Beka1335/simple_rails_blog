bb6dfc559f1fe2a72983c86a3bcb859e7cf84a24
Balu <barishvili@unisens.ge>
2022-07-08 16:58:53 +0400

    correct emale

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,7 +13,8 @@ Rails.application.configure do
   end
 
   config.action_mailer.delivery_method = :letter_opener_web
-  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  #config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  config.action_mailer.default_url_options = { host: 'simple-rails-blog-demo.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   # config.action_mailer.smtp_settings = {
   #   address: '127.0.0.1',
