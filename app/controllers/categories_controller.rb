959e1904a60e4927c9440d469460e2befea51374
Balu <barishvili@unisens.ge>
2022-07-21 10:33:04 +0400

    correct sum bugs

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -7,7 +7,7 @@ class CategoriesController < ApplicationController
 
   # GET /categories or /categories.json
   def index
-    @categories = Category.all
+    @categories = Category.all.paginate(page: params[:page], per_page: 5)
   end
 
   # GET /categories/1 or /categories/1.json
