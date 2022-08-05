c656cd9b96f741376eb89ce4b717c7ffe549dae8
Balu <barishvili@unisens.ge>
2022-08-05 15:02:43 +0400

    correct omniauth error

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -6,40 +6,6 @@ module Users
     before_action :configure_sign_up_params, only: [:create]
     before_action :configure_account_update_params, only: [:update]
 
-    # GET /resource/sign_up
-    # def new
-    #   super
-    # end
-
-    # POST /resource
-    # def create
-    #   super
-    # end
-
-    # GET /resource/edit
-    # def edit
-    #   super
-    # end
-
-    # PUT /resource
-    # def update
-    #   super
-    # end
-
-    # DELETE /resource
-    # def destroy
-    #   super
-    # end
-
-    # GET /resource/cancel
-    # Forces the session data which is usually expired after sign
-    # in to be expired now. This is useful if the user wants to
-    # cancel oauth signing in/up in the middle of the process,
-    # removing all OAuth session data.
-    # def cancel
-    #   super
-    # end
-
     protected
 
     # If you have extra params to permit, append them to the sanitizer.
@@ -58,16 +24,14 @@ module Users
     end
 
     def update_resource(resource, params)
-      resource.update_without_password(params)
-    end
-    # The path used after sign up.
-    # def after_sign_up_path_for(resource)
-    #   super(resource)
-    # end
+      if resource.provider == 'google_oauth2'
+        params.delete('current_password')
+        resource.password = params['password']
 
-    # The path used after sign up for inactive accounts.
-    # def after_inactive_sign_up_path_for(resource)
-    #   super(resource)
-    # end
+        resource.update_without_password(params)
+      else
+        resource.update_with_password(params)
+      end
+    end
   end
 end
